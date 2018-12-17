a9628dce34564d44db85f6b3af6df3270de2b16c
杜厚安 <33805265+DuHouAn@users.noreply.github.com>
2018-12-17 16:30:25 +0800

    Java代码

---
+++ Concurrency/src/LockDemo.java
@@ -0,0 +1,24 @@
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by 18351 on 2018/12/17.
+ */
+public class LockDemo {
+    private static ReentrantLock lock = new ReentrantLock();
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            Thread thread = new Thread(() -> {
+                lock.lock();
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            });
+            thread.start();
+        }
+    }
+}
