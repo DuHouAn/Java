b1742164663a9d8222e2b8a7dc78d3162bbbae49
杜厚安 <33805265+DuHouAn@users.noreply.github.com>
2018-12-21 09:51:44 +0800

    容器概览

---
+++ JavaContainer/src/code_00_container/list/ArrayListTest.java
@@ -0,0 +1,34 @@
+package code_00_container.list;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArrayListTest {
+    public static void main(String[]args){
+        ArrayList<String> list=new ArrayList<String>();
+
+        list.add("aaaa");
+        list.add("bbbb");
+        list.add("cccc");
+        list.add("dddd");
+
+        System.out.println("===========迭代器方式遍历==========");
+        Iterator<String> it=list.iterator();
+        while(it.hasNext()){
+            String s=it.next();
+            System.out.println(s);
+        }
+
+        System.out.println("===========增强for循环方式遍历===========");
+        for(String s:list){
+            System.out.println(s);
+        }
+
+        System.out.println("================================");
+        //根据下标删除元素
+        list.remove(0);
+        for(String s:list){
+            System.out.println(s);
+        }
+    }
+}
