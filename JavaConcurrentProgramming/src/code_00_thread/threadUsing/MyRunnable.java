a3dda8cae77b8fa77db54f0804f6a7986c284ea1
杜厚安 <33805265+DuHouAn@users.noreply.github.com>
2018-12-21 11:30:37 +0800

    Java

--- JavaConcurrentProgramming/src/code_00_thread/threadUsing/MyRunnable.java
+++
@@ -1,8 +0,0 @@
-package code_00_thread.threadUsing;
-
-public class MyRunnable implements Runnable{
-    @Override
-    public void run() {
-        System.out.println("aaaa");
-    }
-}
--- JavaConcurrentProgramming/src/code_00_thread/threadUsing/MyThread.java
+++
@@ -1,7 +0,0 @@
-package code_00_thread.threadUsing;
-
-public class MyThread extends Thread{
-    public void run(){
-        System.out.println("aaaa");
-    }
-}
