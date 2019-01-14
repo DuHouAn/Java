913ef99567ef654efe700731864e813710f4336d
DuHouAn <18351926682@163.com>
2019-01-14 14:54:48 +0800

    面向对象的三大特性

---
+++ Object_Oriented/src/code_03_characters/code_00_encapsulation/StudentDemo.java
@@ -0,0 +1,10 @@
+package code_03_characters.code_00_encapsulation;
+
+/**
+ * Created by 18351 on 2019/1/14.
+ */
+public class StudentDemo {
+    public static void main(String[] args) {
+        Student student=new Student();
+    }
+}
